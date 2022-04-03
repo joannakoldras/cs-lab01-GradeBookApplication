0605a12c9dc9ed1973af8ea7205bfeab5f76393d
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2022-04-03 13:48:53 +0200

    override method  GetLetterGrade in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,8 @@
-using GradeBook.Enums; 
+using GradeBook.Enums;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text; 
 
 namespace GradeBook.GradeBooks
 {
@@ -11,7 +15,27 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            return base.GetLetterGrade(averageGrade); 
+            //return base.GetLetterGrade(averageGrade); 
+            if(studentsNumber < 5)
+            {
+                throw new InvalidOperationException(); 
+            }
+            var count = 0; 
+            for(int i = 0; i < studentsNumber; i++)
+            {
+                if (Students[i].AverageGrade > averageGrade) count++; 
+            }
+            var percentGrade = (count * 100) / studentsNumber;
+            if (percentGrade >= 0 && percentGrade < 20)
+                return 'A';
+            else if (percentGrade >= 20 && percentGrade < 40)
+                return 'B';
+            else if (percentGrade >= 40 && percentGrade < 50)
+                return 'C';
+            else if (percentGrade >= 60 && percentGrade < 80)
+                return 'D';
+            else 
+                return 'F'; 
         }
     }
 }
