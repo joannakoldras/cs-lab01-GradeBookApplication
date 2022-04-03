d687974ae469e57584ff409f55c0af7fa8e6ac95
JoannaKoldras <joannakoldras@microsoft.wsei.edu.pl>
2022-04-03 13:29:08 +0200

    Add RankedGradeBook

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,17 @@
+using GradeBook.Enums; 
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked; 
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade); 
+        }
+    }
+}
